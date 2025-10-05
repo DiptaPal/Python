d9e68ead992bb7a1f1b57c6a806b471091d5ac24
DiptaPal <diptapal1998@gmail.com>
2025-10-05 14:16:26 +0100

    data types

---
+++ datatypes.py
@@ -0,0 +1,59 @@
+# string
+a = "Hello World"
+print(a)
+
+# int
+b = 3
+print(b)
+
+# float
+c = 3.5
+print(c)
+
+# complex
+d = 1j
+print(d)
+
+# list
+e = ["apple", "banana", "orange"]
+print(e)
+
+# tuple
+f = ("apple", "banana", "orange")
+print(f)
+
+# range
+g = range(5)
+print(g)
+
+# dict
+h = {"name" : "dipta", "age" : 27}
+print(h)
+
+# set
+i = {"apple", "banana", "orange"}
+print(i)
+
+# frozenset
+j = frozenset({"apple", "banana", "orange"})
+print(j)
+
+# boolean
+k = True
+print(k)
+
+# bytes
+l = b"hellos"
+print(l)
+
+# bytearray
+q = bytearray(5)
+print(q)
+
+# memoryview
+o = memoryview(bytes(5))
+print(o)
+
+# none
+p = None
+print(p)
